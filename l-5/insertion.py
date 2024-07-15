41d968f3ebb229e297bee51bb18c539b39d4ef35
github-anuk <160622798+github-anuk@users.noreply.github.com>
2024-07-15 16:58:37 +0200

    Add files via upload

---
+++ l-5/insertion.py
@@ -0,0 +1,22 @@
+def insertion(array):
+    for i in range(1,len(array)):
+        k=array[i]
+        j=i-1
+        while j>=0 and k < array[j]:
+            array[j+1]=array[j]
+            j=j-1
+
+        array[j+1]=k
+
+l=[3,1,6,2,8,9]
+insertion(l)
+print(l)
+
+
+            
+         
+
+
+
+
+    
